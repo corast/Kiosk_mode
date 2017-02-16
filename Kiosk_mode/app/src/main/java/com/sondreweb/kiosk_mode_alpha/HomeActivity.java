cd0ee9efc193bf15ffca94315a1bf42f8bc31430
Sondre <corastweb94@hotmail.com>
2017-02-16 20:18:40 +0100

    La til kode for å låse Back button Pressed, slik at vi ikke kommer til gamle Launcher Activity
    Først gang jeg teste, så kunne man få opp gamle launcher activity, noe
    som var merkelig.
    Men det var muligens pga gamle fortsatt kjørte når jeg installerte den
    nye, som ble liksom bare innstalert og kjørt oppå første gang.
    For at det skal fungere opptimalt så må jeg Roote devicen og disable
    gamle launcher.

--- Kiosk_mode/app/src/main/java/com/sondreweb/kiosk_mode_alpha/HomeActivity.java
+++ Kiosk_mode/app/src/main/java/com/sondreweb/kiosk_mode_alpha/HomeActivity.java
@@ -1,8 +1,11 @@
 package com.sondreweb.kiosk_mode_alpha;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,16 +14,46 @@ import android.widget.Toast;
  */
 public class HomeActivity extends Activity {
 
+    public final static String TAG = HomeActivity.class.getSimpleName();
+    private final static String APP = "com.sondreweb.geofencingalpha";
+
+    private PackageManager packageManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //checkIfAppInstalled(this, "testing");
     }
 
     public void showApps(View v) {
         Intent intent = new Intent(this, AppListActivity.class);
         //startActivity(intent);
-        Toast.makeText(this, "Show app virker ikke ....", Toast.LENGTH_SHORT).show();
+
+        if(checkIfAppInstalled(this,APP)){
+            Toast.makeText(this, "Appen GeofencingAlpga er innstallert", Toast.LENGTH_SHORT).show();
+        }else
+        {
+            Toast.makeText(this, "Appen Geofencing er ikke innstallert", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d(TAG,"onBackPressed()");
+        //super.onBackPressed(); //Siden vi er Root Appen så er det ikke vits i å trykke onBack, så den gjør heller ikke noe her ifra.
+    }
+
+    /*Sjekekr om appen er innstalert, vi må altså sjekke om Monumentvandrings appen er installert, ellers så får ikke brukeren gjordt noe.*/
+    public boolean checkIfAppInstalled(Context context, String uri){
+        try{
+            context.getPackageManager().getApplicationInfo(uri,0);
+            return true;
+        }
+        catch (PackageManager.NameNotFoundException e){
+            return false;
+        }
     }
 }
 
